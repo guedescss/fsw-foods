439389e586bc5871a63de5191123947abd59ab24
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-05-04 14:02:37 -0300

    feat: add initial search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,7 +4,7 @@ import { MenuIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between pt-6 ">
+    <div className="flex justify-between px-5 pt-6 ">
       <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
       <Button
         size="icon"
